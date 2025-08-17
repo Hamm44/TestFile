c49252ed5bc06b191aa308b7cb455c8b4fdbc927
Your Name <you@example.com>
2025-08-18 01:35:30 +0400

    forLoo

---
+++ forLoop.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int[] x = new int[100];
+            //int sum = 0;
+            //for (int i = 0; i < x.Length; i++)
+            //{
+            //    x[i] = Random.Shared.Next(1, 100);
+            //    sum += x[i];
+
+            //}
+            //Console.WriteLine(sum);
+
+            int[] x = new int[10];
+            int count = 0;
+            double average = 0;
+            int sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = Random.Shared.Next(1, 10);
+                count++;
+                sum += x[i];
+                average = sum / count;
+
+            }
+            Console.WriteLine(average);
+        }
+    }
+}
+
